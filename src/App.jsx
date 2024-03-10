9bc5f7cbfbd0ce9c647964ee55e6c73016eb5ee7
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-03-10 01:07:40 +0100

    first comiit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-lg font-extrabold'>Dalu</h1>
+      <h2></h2>
+    </div>
+  );
+}
+
+export default App;
