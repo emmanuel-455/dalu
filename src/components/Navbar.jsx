fa100c73374ef9db6265509026b7cb014d3f32c4
emmanuel-455 <120824375+emmanuel-455@users.noreply.github.com>
2024-03-18 23:56:31 +0100

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ function Navbar() {
   return (
     <div className='pr-28 border-r-[0px] '>
       <ul className=' h-screen'>
-        <li className='py-2 text'><a href="">about</a></li>
-        <li className='py-2 text'><a href="/work">works</a></li>
-        <li className='py-2 text'><a href="/media">media diary</a></li>
-        <li className='py-2 text'><a href="">resume</a></li>
-        <li className='py-2 text'><a href="">email</a></li>
+        <li className='py-2 text hover:font-extrabold hover:border-b-[3px] mb-1 w-[50px] border-black'><a href="">about</a></li>
+        <li className='py-2 text hover:font-extrabold hover:border-b-[3px] mb-1 w-[50px] border-black'><a href="/work">works</a></li>
+        <li className='py-2 text hover:font-extrabold hover:border-b-[3px] mb-1 border-black'><a href="/media">media diary</a></li>
+        <li className='py-2 text hover:font-extrabold hover:border-b-[3px] mb-1 w-[60px] border-black'><a href="">resume</a></li>
+        <li className='py-2 text hover:font-extrabold hover:border-b-[3px] mb-1 w-[50px] border-black'><a href="">email</a></li>
       </ul>
     </div>
   )
